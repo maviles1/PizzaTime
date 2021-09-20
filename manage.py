05249f5ecb588177e9b4efd1168c926e52ec030d
maviles1 <mgmbamartin@gmail.com>
2021-09-20 22:19:35 +0200

    daniel a bitch

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PizzaTime.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pizzaTime.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- pizzas/models.py
+++ pizzas/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 
+
 class User(models.Model):
     user_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
 
+
 class Pizza(models.Model):
     pizza_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
+
 
 class Ingredient(models.Model):
     ingredient_id = models.IntegerField(primary_key=True)
@@ -20,9 +23,11 @@ class Pizza_Item(models.Model):
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
 
+
 class Order(models.Model):
     order_id = models.IntegerField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
 class Order_Item(models.Model):
     order_item_id = models.IntegerField(primary_key=True)
