07b6b10f1ad38ba4dafd28c2929eacb0f6e2da92
maviles1 <mgmbamartin@gmail.com>
2021-10-07 02:41:31 +0200

    e

--- pizzaTime/settings.py
+++ pizzaTime/settings.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pizzas'
+    'pizzas',
+    'django_seed'
 ]
 
 MIDDLEWARE = [
@@ -48,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware'
 ]
 
 ROOT_URLCONF = 'pizzaTime.urls'
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'pizzaTime.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -70,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pizzaTime.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -80,7 +79,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -100,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -114,13 +111,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'pizzas.User'
--- pizzas/views.py
+++ pizzas/views.py
@@ -1,31 +1,185 @@
-from django.shortcuts import render
+import string
+from datetime import datetime
+import random
+
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
-from pizzas.models import Dish, Order, Ingredient
+from pizzas.models import Dish, Order, Ingredient, Dish_Item, Order_Item, User, Drink_Dessert, Drink_Dessert_Item, \
+    Discount_Code
 
 
 def menu(request):
-    dishes = Dish.objects.all()
-    return HttpResponse(dishes.size())
+    context = {
+        'dishes': Dish.objects.all(),
+        'cart': active_order(Order.objects.filter(user=request.user), request.user),
+        'drinks': Drink_Dessert.objects.filter(type='1'),
+        'desserts': Drink_Dessert.objects.filter(type='2'),
+        'order_id': active_order(Order.objects.filter(user=request.user), request.user).id
+    }
+    ingredients = Ingredient.objects.all()
+    counter = 1
+    return render(request, 'Menu.html', context)
 
-
-def test(request):
-    if request.method == "POST":
-        pass
-    pass
 
 def dish(request, dish_id):
-    Ingredient(name = "").save()
-    return HttpResponse(Dish.objects.get(pk = dish_id))
+    if request.method == 'GET':
+        context = {
+            'dish': Dish.objects.get(pk=dish_id),
+            'ingredients': Dish.objects.get(pk=dish_id).ingredients.all,
+            'all_ingredients': Ingredient.objects.all,
+        }
+        return render(request, 'Dish.html', context)
+    if request.method == 'POST':
+        current_user = request.user
+        orders = Order.objects.filter(user=current_user)
+        order = active_order(orders, current_user)
+        # extra = Ingredient.objects.get(pk=int(request.POST.get('extra', None)))
+        order_item = Order_Item(dish=Dish.objects.get(pk=dish_id), order=order)
+        order_item.save()
+        # order_item.extras.add(extra)
 
-def add(request):
-    current_user = request.user
-    order = Order.objects.get(user = current_user)
+        # order.dishes.add(Dish.objects.get(pk = dish_id))
+        # order_items = Order_Item.objects.filter(order = order, dish = Dish.objects.get(pk = dish_id))
+        #
+        # if len(order_items) != 0:
+        #     order_item = Order_Item.objects.get(order = order, dish = Dish.objects.get(pk = dish_id))
+        #     order_item.quantity += 1;
+        #     order_item.extras.add(extra)
+        #     order_item.save()
+        return redirect('menu')
 
-def order(request, order_id):
-    current_user = request.user
-    order = Order.objects.get(pk = order_id)
 
-def food(request):
-    pass
+def cart(request, order_id):
+    if request.method == 'POST':
+        order = Order.objects.get(pk=order_id)
+        order_items = Order_Item.objects.filter(order=order)
+        order.status = str(int(order.status) + 1)
+        order.date_created = datetime.now()
+        order.save()
+        return redirect('menu')
+    if request.method == 'GET':
+        order = Order.objects.get(pk=order_id)
+        order_items = Order_Item.objects.filter(order=order)
+        context = {
+            'items': order_items,
+            'order_id': order_id
+        }
+        return render(request, 'Cart.html', context)
+
+
+def my_orders(request):
+    user = request.user
+    orders = Order.objects.filter(user=user)
+    now = datetime.now()
+    for order in Order.objects.filter(status='2'):
+        if (now.replace(tzinfo=None) - order.date_created.replace(tzinfo=None)).total_seconds() > 300:
+            order.status = '3'
+            order.save()
+    if user.eligible_for_discount:
+        code = Discount_Code(user=user, string = generate_code())
+        code.save()
+
+    context = {
+        'user': user,
+        'orders_in_progress': Order.objects.filter(status='2'),
+        'orders_cancelled': Order.objects.filter(status='5'),
+        'orders_delivery': Order.objects.filter(status='3'),
+        'codes': Discount_Code.objects.filter(user = user, active = True)
+    }
+    return render(request, 'Orders.html', context)
+
+
+def cancel_order(request, order_id):
+    order = Order.objects.get(pk=order_id)
+    order.status = '5'
+    order.save()
+    return redirect('orders')
+
+
+def extras(request, dish_id):
+    context = {
+
+    }
+    return render(request, 'Extra.html', context)
+
+
+def active_order(orders, current_user):
+    for order in orders:
+        if order.status == '1':
+            return order
+    o = Order(user=current_user)
+    o.save()
+    return o
+
+
+def home(request):
+    context = {'form': UserCreationForm()}
+    return render(request, 'Home.html', context)
+
+
+def confirmation(request, order_id):
+    if request.method == 'POST':
+        if is_valid(request.POST['code']) or not request.POST['code']:
+            order = Order.objects.get(pk=order_id)
+            if request.POST['code'] != '':
+                code = get_code(request.POST['code'])
+                code.order = order
+                code.active = False
+                code.save()
+            order_items = Order_Item.objects.filter(order=order)
+            order.status = str(int(order.status) + 1)
+            order.date_created = datetime.now()
+            order.save()
+            context = {'order_id': order_id,
+                       'order': Order.objects.get(pk=order_id)}
+            return render(request, 'Confirmation.html', context)
+        return redirect('finalize', order_id=order_id)
+    if request.method == 'GET':
+        return redirect('orders')
+
+
+def drink_dessert(request, drink_dessert_id):
+    if request.method == 'POST':
+        current_user = request.user
+        orders = Order.objects.filter(user=current_user)
+        order = active_order(orders, current_user)
+        # extra = Ingredient.objects.get(pk=int(request.POST.get('extra', None)))
+        drink_dessert_item = Drink_Dessert_Item(drink_dessert=Drink_Dessert.objects.get(pk=drink_dessert_id),
+                                                order=order)
+        drink_dessert_item.save()
+        return redirect('menu')
+
+
+def finalize(request, order_id):
+    if not Order.objects.get(pk=order_id).valid:
+        return redirect('menu')
+    context = {
+        'order_id': order_id
+    }
+    return render(request, 'Finalize.html', context)
+
+
+def is_valid(code_str):
+    codes = Discount_Code.objects.filter(string=code_str)
+    for code in codes:
+        if code.string == code_str and code.active:
+            return True
+    return False
+
+
+def get_code(code_str):
+    codes = Discount_Code.objects.filter(string=code_str)
+    for code in codes:
+        if code.string == code_str and code.active:
+            return code
+
+
+def generate_code():
+    letters = string.ascii_lowercase
+    str = ''
+    for i in range(0,6):
+        str += letters[random.randint(0,len(letters)-1)]
+    return str
